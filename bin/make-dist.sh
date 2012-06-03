0fb10d230b34ef46e4143dd00ff795b7f20408f2
Peter Bakota <bakota@gmail.com>
2012-06-03 20:42:35 +0200

    Changed git archive format to zip. Removed AmigaDX.def

--- bin/make-dist.sh
+++ bin/make-dist.sh
@@ -3,9 +3,9 @@
 HERE=$(dirname $(readlink -f "$0"))
 
 cd ${HERE}/../install
-zip -9 ${HERE}/../amigadx36_x86_64.bin.zip *
+zip -9 ${HERE}/../amigadx36.bin.zip *
 cd ${HERE}/../
 
-ARCHIVE=amigadx.src.tgz
-git archive --format=tar.gz HEAD >${ARCHIVE}
+ARCHIVE=amigadx36.src.zip
+git archive --format=zip HEAD >${ARCHIVE}
 
